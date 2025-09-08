aacbc3245d3e1e880b183760c0dd2b6931886281
sosallycanwait <andrew00874@gmail.com>
2025-09-08 15:16:35 +0900

    IO- 입출력

---
+++ src/edu/practice/exception/ExceptionRun.java
@@ -0,0 +1,9 @@
+package edu.practice.exception;
+
+public class ExceptionRun {
+    public static void main(String[] args) {
+        ExceptionService ex = new ExceptionService();
+//        ex.method1();
+        ex.method3();
+    }
+}
